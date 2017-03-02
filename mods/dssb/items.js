bbb482fc4335bbf3e7bfa8ebf584869e6bc4ef92
Insist <DeathlyPlays@users.noreply.github.com>
2017-03-02 13:42:00 -0600

    Remove it

--- mods/dssb/items.js
+++ mods/dssb/items.js
@@ -13,24 +13,4 @@ exports.BattleItems = {
 		gen: -1,
 		desc: "If holder is a Ludicolo with Aqua Subscribe, it can use Exiled From All Others.",
 	},
-	"encouragementstone": {
-		spritenum: 307,
-		id: "encouragementstone",
-		name: "Encouragement Stone",
-		num: -2,
-		gen: -2,
-		fling: {
-			basePower: 40,
-		},
-		onModifyDamage: function (damage, source, target, move) {
-			if (move && move.typeMod > 0) {
-				return this.chainModify([0x1333, 0x1000]);
-			}
-		},
-		onSwitchIn: function(pokemon) {
-			this.boost({
-				atk: 1
-			});
-		},
-	},
 };
