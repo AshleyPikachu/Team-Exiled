41e40040933f8ab8bce5c53e09bc56f32a4a1b18
Insist <DeathlyPlays@users.noreply.github.com>
2017-03-03 06:55:40 -0600

    Remove for now

--- rooms.js
+++ rooms.js
@@ -563,21 +563,6 @@ class GlobalRoom {
 
 		if (!this.searches[formatid]) this.searches[formatid] = [];
 		let formatSearches = this.searches[formatid];
-
-		if (Tools.getFormat(formatid).isWildEncounter) {
-			delete user.searching[formatid];
-
-			if (!Users('sgserver')) {
-				SG.makeCOM();
-
-			}
-			let wildTeam = SG.makeWildPokemon();
-			Users('sgserver').wildTeams[user.userid] = wildTeam;
-			this.startBattle(Users('sgserver'), user, formatid, wildTeam, newSearch.team, {
-				rated: false
-			});
-			return;
-		}
 		// Prioritize players who have been searching for a match the longest.
 		for (let i = 0; i < formatSearches.length; i++) {
 			let search = formatSearches[i];
