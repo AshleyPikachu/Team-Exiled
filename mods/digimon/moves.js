2463e71b7b7095f6b7ee372c6529b25c55c3b41f
Insist <DeathlyPlays@users.noreply.github.com>
2017-03-03 20:10:14 -0600

    More Digimon moves -Fifty Nine (Edits from me)

--- mods/digimon/moves.js
+++ mods/digimon/moves.js
@@ -32,4 +32,98 @@ exports.BattleMovedex = {
         accuracy: 100,
         type: "Fire",
     },
+    "prominencebeam": {
+        id: "prominencebeam",
+        name: "Prominence Beam",
+        basePower: 444,
+        category: "Special",
+        secondary: {
+            self: {
+                chance: 20,
+                boosts: {
+                    spa: -2,
+                    atk: -2
+                }
+            },
+        },
+        priority: 0,
+        target: "any",
+        pp: 5,
+        flags: {
+            protect: 1
+        },
+        accuracy: 100,
+        type: "Fire",
+    },
+    "spitfire": {
+        id: "spitfire",
+        name: "Spit Fire",
+        basePower: 66,
+        category: "Special",
+        secondary: false,
+        priority: 0,
+        target: "any",
+        pp: 25,
+        flags: {
+            protect: 1
+        },
+        accuracy: 100,
+        type: "Fire",
+    },
+    "redinferno": {
+        id: "redinferno",
+        name: "Red Inferno",
+        basePower: 210,
+        category: "Special",
+        secondary: false,
+        priority: 0,
+        target: "any",
+        pp: 15,
+        flags: {
+            protect: 1
+        },
+        accuracy: 100,
+        type: "Fire",
+    },
+    "magmabomb": {
+        id: "magmabomb",
+        name: "Magma Bomb",
+        basePower: 279,
+        category: "Physical",
+        secondary: {
+            chance: 25,
+            volatileStatus: "confusion"
+        },
+        priority: 0,
+        target: "any",
+        pp: 15,
+        flags: {
+            protect: 1
+        },
+        accuracy: 100,
+        type: "Fire",
+    },
+    "heatlaser": {
+        id: "heatlaser",
+        name: "Heat Laser",
+        basePower: 84,
+        category: "Special",
+        secondary: {
+            self: {
+                chance: 10,
+                boosts: {
+                    spa: -1,
+                    atk: -1
+                }
+            },
+        },
+        priority: 0,
+        target: "any",
+        pp: 30,
+        flags: {
+            protect: 1
+        },
+        accuracy: 100,
+        type: "Fire",
+    },
 };
