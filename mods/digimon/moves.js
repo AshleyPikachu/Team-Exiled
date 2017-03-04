1b26de70addd61b3c1f091b7905c81d604721dbc
Insist <DeathlyPlays@users.noreply.github.com>
2017-03-03 19:45:19 -0600

    Add Fire Tower - Fifty Nine

--- mods/digimon/moves.js
+++ mods/digimon/moves.js
@@ -14,4 +14,22 @@ exports.BattleMovedex = {
         accuracy: 100,
         type: "Ice",
     },
+    "firetower": {
+        id: "firetower",
+        name: "Fire Tower",
+        basePower: 155,
+        category: "Physical",
+        secondary: {
+            chance: 25,
+            volatileStatus: "flinch"
+        },
+        priority: 0,
+        target: "any",
+        pp: 20,
+        flags: {
+            protect: 1
+        },
+        accuracy: 100,
+        type: "Fire",
+    },
 };
