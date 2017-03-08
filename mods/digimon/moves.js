3474b52e2114e87c15fb5dbfedd56a3a3efe5495
Insist <DeathlyPlays@users.noreply.github.com>
2017-03-08 08:12:57 -0600

    Finished adding all the Battle type moves -Insist

--- mods/digimon/moves.js
+++ mods/digimon/moves.js
@@ -109,13 +109,11 @@ exports.BattleMovedex = {
         basePower: 84,
         category: "Special",
         secondary: {
-            self: {
-                chance: 10,
-                boosts: {
-                    spa: -1,
-                    atk: -1
-                }
-            },
+            chance: 10,
+            boosts: {
+                spa: -1,
+                atk: -1
+            }
         },
         priority: 0,
         target: "any",
@@ -195,4 +193,124 @@ exports.BattleMovedex = {
         flags: {},
         type: "Battle",
     },
+    "warcry": {
+        id: "warcry",
+        name: "War Cry",
+        basePower: 0,
+        secondary: false,
+        category: "Status",
+        pp: 10,
+        accuracy: 100,
+        boosts: {
+            atk: 1,
+            def: 1,
+            spa: 1,
+            spd: 1,
+            spe: 1
+        },
+        priority: 0,
+        flags: {},
+        target: "self",
+        type: "Battle",
+    },
+    "sonicjab": {
+        id: "sonicjab",
+        name: "Sonic Jab",
+        basePower: 52,
+        category: "Physical",
+        accuracy: 100,
+        secondary: false,
+        priority: 0,
+        flags: {},
+        pp: 15,
+        target: "normal",
+        type: "Battle",
+    },
+    "dynamitekick": {
+        id: "dynamitekick",
+        name: "Dynamite Kick",
+        basePower: 193,
+        accuracy: 100,
+        pp: 5,
+        category: "Special",
+        volatileStatus: "flinch",
+        secondary: false,
+        priority: 0,
+        flags: {},
+        target: "normal",
+        type: "Battle",
+    },
+    "counter": {
+        id: "counter",
+        name: "Counter",
+        basePower: 285,
+        volatileStatus: "confusion",
+        category: "Physical",
+        damageCallback: function (pokemon) {
+			if (!pokemon.volatiles['counter']) return 0;
+			return pokemon.volatiles['counter'].damage || 1;
+		},
+		category: "Physical",
+		pp: 20,
+		priority: -5,
+		flags: {},
+		beforeTurnCallback: function (pokemon) {
+			pokemon.addVolatile('counter');
+		},
+		onTryHit: function (target, source, move) {
+			if (!source.volatiles['counter']) return false;
+			if (source.volatiles['counter'].position === null) return false;
+		},
+		effect: {
+			duration: 1,
+			noCopy: true,
+			onStart: function (target, source, source2, move) {
+				this.effectData.position = null;
+				this.effectData.damage = 0;
+			},
+			onRedirectTargetPriority: -1,
+			onRedirectTarget: function (target, source, source2) {
+				if (source !== this.effectData.target) return;
+				return source.side.foe.active[this.effectData.position];
+			},
+			onDamagePriority: -101,
+			onDamage: function (damage, target, source, effect) {
+				if (effect && effect.effectType === 'Move' && source.side !== target.side && this.getCategory(effect) === 'Physical') {
+					this.effectData.position = source.position;
+					this.effectData.damage = 2 * damage;
+				}
+			},
+		},
+		secondary: false,
+		target: "normal",
+		type: "Battle",
+	},
+	"megatonpunch": {
+	    id: "megatonpunch",
+	    name: "Megaton Punch",
+	    basePower: 320,
+	    category: "Physical",
+	    accuracy: 100,
+	    pp: 5,
+	    volatileStatus: "flinch",
+	    secondary: false,
+	    flags: {},
+	    priority: 0,
+	    target: "normal",
+	    type: "Battle",
+	},
+	"busterdrive": {
+	    id: "busterdrive",
+	    name: "Buster Drive",
+	    basePower: 500,
+	    volatileStatus: "confusion",
+	    category: "Physical",
+	    pp: 5,
+	    accuracy: 100,
+	    secondary: false,
+	    flags: {},
+	    priority: 0,
+	    target: "allAdjacent",
+	    type: "Battle",
+	},
 };
