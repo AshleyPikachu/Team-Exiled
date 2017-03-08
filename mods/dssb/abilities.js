57cb8c37eb13655cba71420e23eb6381254f29f4
Insist <DeathlyPlays@users.noreply.github.com>
2017-03-08 07:47:08 -0600

    Fix Xurk's and 59's Abiility

--- mods/dssb/abilities.js
+++ mods/dssb/abilities.js
@@ -6,17 +6,17 @@ exports.BattleAbilities = {
 		id: "cripplingdepression",
 		name: "Crippling Depression",
 		//primordialseas
-		onStart: function(source) {
+		onStart: function (source) {
 			this.setWeather('primordialsea');
 		},
-		onAnySetWeather: function(target, source, weather) {
+		onAnySetWeather: function (target, source, weather) {
 			if (this.getWeather().id === 'primordialsea' && !(weather.id in {
 					desolateland: 1,
 					primordialsea: 1,
 					deltastream: 1
 				})) return false;
 		},
-		onEnd: function(pokemon) {
+		onEnd: function (pokemon) {
 			if (this.weatherData.source !== pokemon) return;
 			for (let i = 0; i < this.sides.length; i++) {
 				for (let j = 0; j < this.sides[i].active.length; j++) {
@@ -31,47 +31,27 @@ exports.BattleAbilities = {
 			this.clearWeather();
 		},
 		//raindish
-		onWeather: function(target, source, effect) {
+		onWeather: function (target, source, effect) {
 			if (effect.id === 'raindance' || effect.id === 'primordialsea') {
 				this.heal(target.maxhp / 16);
 			}
 		},
 		//swiftswim
-		onModifySpe: function(spe, pokemon) {
+		onModifySpe: function (spe, pokemon) {
 			if (this.isWeather(['raindance', 'primordialsea'])) {
 				return this.chainModify(2);
 			}
 		},
 	},
-	"feelsflys": {
-		id: "feelsflys",
-		name: "feelsflys",
-		onSourceFaint: function (target, source, effect) {
-			if (effect && effect.effectType === 'Move') {
-				let stat = 'atk';
-				let bestStat = 0;
-				for (let i in source.stats) {
-					if (source.stats[i] > bestStat) {
-						stat = i;
-						bestStat = source.stats[i];
-					}
-				}
-				this.boost({[stat]:1}, source);
-			}
-		},
-		onStart: function (pokemon) {
-			this.useMove('Magnet Rise', pokemon);
-		},	
-	},
 	"prfmlmao": {
 		id: "prfmlmao",
 		name: "prfmlmao",
-		onStart: function(source) {
+		onStart: function (source) {
 			this.setWeather('hail');
 		},
 		//pixilate
 		onModifyMovePriority: -1,
-		onModifyMove: function(move, pokemon) {
+		onModifyMove: function (move, pokemon) {
 			if (move.type === 'Normal' && move.id !== 'naturalgift') {
 				move.type = 'Fairy';
 				if (move.category !== 'Status') pokemon.addVolatile('pixilate');
@@ -80,18 +60,18 @@ exports.BattleAbilities = {
 		effect: {
 			duration: 1,
 			onBasePowerPriority: 8,
-			onBasePower: function(basePower, pokemon, target, move) {
+			onBasePower: function (basePower, pokemon, target, move) {
 				return this.chainModify([0x14CD, 0x1000]);
 			},
 		},
 		//hail solar power
 		onModifySpAPriority: 5,
-		onModifySpA: function(spa, pokemon) {
+		onModifySpA: function (spa, pokemon) {
 			if (this.isWeather(['hail'])) {
 				return this.chainModify(1.5);
 			}
 		},
-		onWeather: function(target, source, effect) {
+		onWeather: function (target, source, effect) {
 			if (effect.id === 'hail') {
 				this.damage(target.maxhp / 8, target, target);
 			}
@@ -101,7 +81,7 @@ exports.BattleAbilities = {
 		id: "brilliantscale",
 		name: "Brilliant Scale",
 		//multiscale + supereffective moves only do half as much
-		onSourceModifyDamage: function(damage, source, target, move) {
+		onSourceModifyDamage: function (damage, source, target, move) {
 			if (target.hp >= target.maxhp) {
 				this.debug('Brilliant Scale weaken');
 				return this.chainModify(0.5);
@@ -112,7 +92,7 @@ exports.BattleAbilities = {
 			}
 		},
 		//tintedlens
-		onModifyDamage: function(damage, source, target, move) {
+		onModifyDamage: function (damage, source, target, move) {
 			if (move.typeMod < 0) {
 				this.debug('Brilliant Scale boost');
 				return this.chainModify(2);
@@ -122,14 +102,14 @@ exports.BattleAbilities = {
 	"mosmicpower": {
 		id: "mosmicpower",
 		name: "Mosmic Power",
-		onStart: function(pokemon) {
+		onStart: function (pokemon) {
 			this.boost({
 				atk: 1,
 				spa: 1,
 				spe: 1
 			});
 		},
-		onAnyTryPrimaryHit: function(target, source, move) {
+		onAnyTryPrimaryHit: function (target, source, move) {
 			if (target === source || move.category === 'Status') return;
 			if (move.type === 'Dark') {
 				source.addVolatile('aura');
@@ -139,17 +119,17 @@ exports.BattleAbilities = {
 	"solarrule": {
 		id: "solarrule",
 		name: "Solar Rule",
-		onStart: function(source) {
+		onStart: function (source) {
 			this.setWeather('desolateland');
 		},
-		onAnySetWeather: function(target, source, weather) {
+		onAnySetWeather: function (target, source, weather) {
 			if (this.getWeather().id === 'desolateland' && !(weather.id in {
 					desolateland: 1,
 					primordialsea: 1,
 					deltastream: 1
 				})) return false;
 		},
-		onEnd: function(pokemon) {
+		onEnd: function (pokemon) {
 			if (this.weatherData.source !== pokemon) return;
 			for (let i = 0; i < this.sides.length; i++) {
 				for (let j = 0; j < this.sides[i].active.length; j++) {
@@ -164,14 +144,39 @@ exports.BattleAbilities = {
 			this.clearWeather();
 		},
 		onModifyAtkPriority: 5,
-		onModifyAtk: function(atk, pokemon) {
+		onModifyAtk: function (atk, pokemon) {
 			if (this.isWeather(['sunnyday', 'desolateland'])) {
 				return this.chainModify(1.5);
 			}
 		},
-		onWeather: function(target, source, effect) {
+		onWeather: function (target, source, effect) {
 			if (effect.id === 'sunnyday' || effect.id === 'desolateland') {
 				this.damage(target.maxhp / 8, target, target);
+			}
+		},
+	},
+	"feelsflys": {
+		id: "feelsflys",
+		name: "feelsflys",
+		onStart: function (pokemon) {
+			this.useMove('magnetrise', pokemon);
+			this.boost({
+				spa: 2
+			});
+		},
+		onSourceFaint: function (target, source, effect) {
+			if (effect && effect.effectType === 'Move') {
+				let stat = 'atk';
+				let bestStat = 0;
+				for (let i in source.stats) {
+					if (source.stats[i] > bestStat) {
+						stat = i;
+						bestStat = source.stats[i];
+					}
+				}
+				this.boost({
+					[stat]: 1
+				}, source);
 			}
 		},
 	},
@@ -186,6 +191,15 @@ exports.BattleAbilities = {
 		},
 		onModifyPriority: function (priority, pokemon, target, move) {
 			if (move && move.type === 'Fairy') return priority + 1;
+			if (move && move.type === 'Normal') return priority + 1;
+		},
+		onPrepareHit: function (source, target, move) {
+			if (move.hasBounced) return;
+			let type = move.type;
+			if (type && type !== '???' && source.getTypes().join() !== type) {
+				if (!source.setType(type)) return;
+				this.add('-start', source, 'typechange', type, '[from] Hypothesis');
+			}
 		},
 	},
 };
