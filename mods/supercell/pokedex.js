4359c164222e193ef32ca8b6e0990231236afe55
Insist <DeathlyPlays@users.noreply.github.com>
2017-03-09 16:41:13 -0600

    Fix minor typo + add more

--- mods/supercell/pokedex.js
+++ mods/supercell/pokedex.js
@@ -14,7 +14,7 @@ exports.BattlePokedex = {
             spe: 90
         },
         abilities: {
-            "0": "Adaptability"
+            0: "Adaptability"
         },
         heightm: 1.2,
         weightkg: 38,
@@ -32,7 +32,7 @@ exports.BattlePokedex = {
             spe: 110
         },
         abilities: {
-            "0": "Sharpshooter"
+            0: "Sharpshooter"
         },
         heightm: 1.2,
         weightkg: 6.3,
@@ -50,7 +50,7 @@ exports.BattlePokedex = {
             spe: 145
         },
         abilities: {
-            "0": "Theft"
+            0: "Theft"
         },
         heightm: 0.6,
         weightkg: 4.2,
@@ -68,7 +68,7 @@ exports.BattlePokedex = {
             spe: 5
         },
         abilities: {
-            "0": "Meatshield"
+            0: "Meatshield"
         },
         heightm: 5.7,
         weightkg: 120.0,
@@ -86,7 +86,7 @@ exports.BattlePokedex = {
             spe: 135
         },
         abilities: {
-            "0": "Wizardry"
+            0: "Wizardry"
         },
         heightm: 1.6,
         weightkg: 6.4,
@@ -104,7 +104,7 @@ exports.BattlePokedex = {
             spe: 25
         },
         abilities: {
-            "0": "I Gotz a Bomb"
+            0: "I Gotz a Bomb"
         },
         heightm: 4.8,
         weightkg: 0.8,
@@ -122,7 +122,7 @@ exports.BattlePokedex = {
             spe: 145
         },
         abilities: {
-            "0": "Mayhem"
+            0: "Mayhem"
         },
         heightm: 0.8,
         weightkg: 1.2,
@@ -140,7 +140,7 @@ exports.BattlePokedex = {
             spe: 130
         },
         abilities: {
-            "0": "HOG RIDDAH"
+            0: "HOG RIDDAH"
         },
         heightm: 3.7,
         weightkg: 32.6,
@@ -158,9 +158,63 @@ exports.BattlePokedex = {
             spe: 45
         },
         abilities: {
-            "0": "Witchcraft"
+            0: "Witchcraft"
         },
         heightm: 2.2,
         weightkg: 8.7,
+    },
+    lavahound: {
+        num: -209,
+        species: "Lava Hound",
+        types: ["Fire", "Flying"],
+        baseStats: {
+            hp: 95,
+            atk: 70,
+            def: 95,
+            spa: 80,
+            spd: 95,
+            spe: 40
+        },
+        abilities: {
+            0: "Burnout"
+        },
+        heightm: 4.8,
+        weightkg: 46.8,
+    },
+    lavapup: {
+        num: -209,
+        species: "Lava Pup",
+        types: ["Fire", "Flying"],
+        baseStats: {
+            hp: 25,
+            atk: 30,
+            def: 20,
+            spa: 60,
+            spd: 20,
+            spe: 105
+        },
+        abilities: {
+            0: "Burnout"
+        },
+        heightm: 0.2,
+        weightkg: 0.1,
+    },
+    pekka: {
+        num: -210,
+        species: "PEKKA",
+        types: ["Steel", "Dark"],
+        baseStats: {
+            hp: 120,
+            atk: 120,
+            def: 120,
+            spa: 40,
+            spd: 60,
+            spe: 5
+        },
+        abilities: {
+            0: "Masked Warrior"
+        },
+        heightm: 6.7,
+        weightkg: 253.8,
     },
 };
